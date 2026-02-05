785e18149548ced13565e422c0018e3e9ec6bf8c
Mcpike <3013592m@student.gla.ac.uk>
2026-02-05 17:10:55 +0000

    Now use slugging for URLS, new catergory.html working

--- rango/admin.py
+++ rango/admin.py
@@ -4,8 +4,10 @@ from rango.models import Category, Page
 # Define how you want the Page list to look
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
+class CategoryAdmin (admin.ModelAdmin):
+    prepopulated_fields ={'slug':('name',)}
 
-# Register the models
-admin.site.register(Category)
+
 # Pass PageAdmin as the second argument here!
 admin.site.register(Page, PageAdmin)
+admin.site.register(Category, CategoryAdmin)
