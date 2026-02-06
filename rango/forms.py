a5cd86b3ca488d27abe2c1554cb4ae580d6c1595
Mcpike <3013592m@student.gla.ac.uk>
2026-02-06 13:41:09 +0000

    Add page feature now functional, chapter 7 complete

--- rango/forms.py
+++ rango/forms.py
@@ -20,11 +20,22 @@ class PageForm(forms.ModelForm):
     url = forms.URLField(max_length=200,
                          help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
+    def clean(self):
+        cleaned_data = self.cleaned_data
+        url = cleaned_data.get('url')
+
+        # If url is not empty and doesn't start with http://, prepend it.
+        if url and not url.startswith('http://'):
+            url = f'http://{url}'
+            cleaned_data['url'] = url
+
+        return cleaned_data
     class Meta:
     # Provide an association between the ModelForm and a model model = Page
     # What fields do we want to include in our form?
     # This way we don't need every field in the model present.
     # Some fields may allow NULL values; we may not want to include them. # Here, we are hiding the foreign key.
     # we can either exclude the category field from the form,
-        exclude = ('category',)
+        model = Page
+        exclude = ('category','views',)
 # or specify the fields to include (don't include the category field). #fields = ('title', 'url', 'views')
